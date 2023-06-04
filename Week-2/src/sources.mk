d5e44a70d5593b2f67f4dafe0890e3057c531dcf
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2023-06-05 00:57:46 +0530

    adding makefile and sources.mk

--- Week-2/src/sources.mk
+++ Week-2/src/sources.mk
@@ -9,9 +9,26 @@
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
+ifeq ($(PLATFORM),MSP432)
+	SOURCES =          main.c \
+			   memory.c \
+			   interrupts_msp432p401r_gcc.c \
+			   startup_msp432p401r_gcc.c \
+			   system_msp432p401r.c
+else
+	SOURCES = main.c \
+		  memory.c 
+
+	endif
 
 # Add your include paths to this variable
-INCLUDES = 
+# Add your include paths to this variable
+ifeq ($(PLATFORM),MSP432)
+	INCLUDES = 	   -I../include/CMSIS \
+			   -I../include/common \
+			   -I../include/msp432 \
+			   -I../ 
+else
+	INCLUDES =         -I../include/common	
 
+endif
